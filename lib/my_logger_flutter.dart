2b0fdf96363d2725ae346828606dfccecd800c64
ThinhDac <thinhnd@watermelon.vn>
2024-09-30 14:08:53 +0700

    update code add screen report error

--- lib/my_logger_flutter.dart
+++ lib/my_logger_flutter.dart
@@ -1,3 +1,4 @@
 library my_logger_flutter;
 
 export 'src/logger.dart';
+export 'src/popup_report.dart';
---
+++ lib/src/popup_report.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class TextAreaPopup {
+  static void show({
+    required BuildContext context,
+    required Function(String) onSubmit,
+    String? hintTextField,
+    String? textButtonCancel,
+    String? textButtonSend,
+    String? textTitle,
+  }) {
+    TextEditingController _controller = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(textTitle ?? 'Mô tả lỗi'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              // Input TextArea để người dùng mô tả lỗi
+              TextField(
+                controller: _controller,
+                maxLines: 5,
+                decoration: InputDecoration(
+                  hintText: hintTextField ?? 'Vui lòng mô tả lỗi...',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Đóng popup
+              },
+              child: Text(textButtonCancel ?? 'Hủy'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Gọi hàm được truyền vào khi nhấn nút
+                onSubmit(_controller.text);
+                Navigator.of(context).pop(); // Đóng popup
+              },
+              child: Text(textButtonSend ?? 'Gửi'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
